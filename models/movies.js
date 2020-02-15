f493c652fe3bbc64f0369572d351629aef3a3da9
Abhishek Jain <abhishekjai35@gmail.com>
2020-02-15 22:25:02 +0530

    Added route for movies and Added validation for movies

--- models/movies.js
+++ models/movies.js
@@ -13,20 +13,41 @@ const Movie = mongoose.model(
       maxlength: 255
     },
     genre: {
-        type: genreSchema,
-        required: true
+      type: genreSchema,
+      required: true
     },
     numberInStock: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 255
+      type: Number,
+      required: true,
+      min: 0,
+      max: 255
     },
     dailyRentalRate: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 255
+      type: Number,
+      required: true,
+      min: 0,
+      max: 255
     }
   })
 );
+
+function validateMovie(movie) {
+  const schema = {
+    title: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    genreId: Joi.string().required(),
+    numberInStock: Joi.number()
+      .min(0)
+      .required(),
+    dailyRentalRate: Joi.number()
+      .min(0)
+      .required()
+  };
+
+  return Joi.validate(movie, schema);
+}
+
+exports.Movie = Movie;
+exports.validate = validateMovie;
