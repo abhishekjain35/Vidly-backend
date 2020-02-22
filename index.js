c4d90c4aa9a40b85da0ae2628c422fdc1469ff39
Abhishek Jain <abhishekjai35@gmail.com>
2020-02-22 15:25:05 +0530

    Implemented error handler in middleware function

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const rentals = require("./routes/rentals");
 const movies = require("./routes/movies");
 const users = require("./routes/users");
 const auth = require("./routes/auth");
+const error = require('./middleware/error')
 const express = require("express");
 const app = express();
 
@@ -31,6 +32,7 @@ app.use("/api/rentals", rentals);
 app.use("/api/movies", movies);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+app.use(error)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
