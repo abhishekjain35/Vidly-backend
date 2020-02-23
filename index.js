f372c1abddaf5bd7f527c780c559b4f456b914ed
Abhishek Jain <abhishekjai35@gmail.com>
2020-02-23 19:44:46 +0530

    Refactored routes into separate module

--- index.js
+++ index.js
@@ -4,15 +4,9 @@ require("express-async-errors");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const rentals = require("./routes/rentals");
-const movies = require("./routes/movies");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const error = require("./middleware/error");
 const express = require("express");
 const app = express();
+require("./startup/routes")(app);
 
 process.on("uncaughtException", err => {
   winston.error(err.message, err);
@@ -38,15 +32,6 @@ mongoose
   })
   .then(() => console.log("Connected to MongoDB..."))
   .catch(err => console.error("Could not connect to MongoDB..."));
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/rentals", rentals);
-app.use("/api/movies", movies);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
