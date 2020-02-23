64ed7a9dfcfe6abafd518b594857716ded898bf5
Abhishek Jain <abhishekjai35@gmail.com>
2020-02-23 20:17:29 +0530

    Refactored config and validation login in separate modules

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-const config = require("config");
 const express = require("express");
 const app = express();
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
 
-require("./startup/logging");
+require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
-  process.exit(1);
-}
+require("./startup/config")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
