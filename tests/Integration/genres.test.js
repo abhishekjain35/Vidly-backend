426f013374ee766665a467708e6ec8accbbed4b8
Abhishek Jain <abhishekjai35@gmail.com>
2020-02-24 21:12:33 +0530

    Added integration test with the database

--- tests/Integration/genres.test.js
+++ tests/Integration/genres.test.js
@@ -1,17 +1,26 @@
-const request = require('supertest')
+const request = require("supertest");
+const { Genre } = require("../../models/genre");
 let server;
 
 describe("/api/genres", () => {
   beforeEach(() => {
     server = require("../../index");
   });
-  afterEach(() => {
+  afterEach(async () => {
     server.close();
+    await Genre.remove({});
   });
   describe("GET /", () => {
-    it("should return all genres",async () => {
-        const res = await request(server).get('/api/genres')
-        expect(res.status).toBe(200)
+    it("should return all genres", async () => {
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" }
+      ]);
+      const res = await request(server).get("/api/genres");
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some(g => g.name === "genre1")).toBeTruthy();
+      expect(res.body.some(g => g.name === "genre2")).toBeTruthy();
     });
   });
 });
