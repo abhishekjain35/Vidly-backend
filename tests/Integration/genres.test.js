cc7122df2907178b7eeaf99f62ee98df8476f21a
Abhishek Jain <abhishekjai35@gmail.com>
2020-02-24 22:00:52 +0530

    Implemented integration test for invalid id of genres endpoint

--- tests/Integration/genres.test.js
+++ tests/Integration/genres.test.js
@@ -30,8 +30,15 @@ describe("/api/genres", () => {
       await genre.save();
 
       const res = await request(server).get("/api/genres/" + genre._id);
-      expect(res.status).toBe(200)
-      expect(res.body).toHaveProperty('name', genre.name)
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("name", genre.name);
+    });
+  });
+
+  describe("GET /:id", () => {
+    it("should return 404 if invalid id is passed", async () => {
+      const res = await request(server).get("/api/genres/1");
+      expect(res.status).toBe(404);
     });
   });
 });
