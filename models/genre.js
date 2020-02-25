797b77131877e0f96695296b1a3b08078805de0e
Abhishek Jain <abhishekjai35@gmail.com>
2020-02-25 19:56:14 +0530

    Implemented integration testing for invalid inputs

--- models/genre.js
+++ models/genre.js
@@ -14,7 +14,8 @@ const Genre = mongoose.model("Genre", genreSchema);
 function validateGenre(genre) {
   const schema = {
     name: Joi.string()
-      .min(3)
+      .min(5)
+      .max(50)
       .required()
   };
 
@@ -23,4 +24,4 @@ function validateGenre(genre) {
 
 exports.Genre = Genre;
 exports.validate = validateGenre;
-exports.genreSchema = genreSchema
+exports.genreSchema = genreSchema;
--- tests/Integration/genres.test.js
+++ tests/Integration/genres.test.js
@@ -1,5 +1,7 @@
 const request = require("supertest");
 const { Genre } = require("../../models/genre");
+const { User } = require("../../models/users");
+
 let server;
 
 describe("/api/genres", () => {
@@ -48,6 +50,26 @@ describe("/api/genres", () => {
         .post("/api/genres")
         .send({ name: "genre1" });
       expect(res.status).toBe(401);
+    });
+
+    it("should return 400 if genre is less than 5 characters", async () => {
+      const token = new User().generateAuthToken();
+
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: "1234" });
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if genre is more than 50 characters", async () => {
+      const token = new User().generateAuthToken();
+      const name = new Array(52).join('a')
+      const res = await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: name });
+      expect(res.status).toBe(400);
     });
   });
 });
