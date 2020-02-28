561b83c1c8dc3b16f049591ac8bcc176619ed038
Abhishek Jain <abhishekjai35@gmail.com>
2020-02-28 19:52:57 +0530

    TDD for returns api to give 401 error if client is not logged in

--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,11 @@
-const express = require('express')
+const express = require("express");
 const genres = require("../routes/genres");
 const customers = require("../routes/customers");
 const rentals = require("../routes/rentals");
 const movies = require("../routes/movies");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
+const returns = require("../routes/returns");
 const error = require("../middleware/error");
 
 module.exports = function(app) {
@@ -15,5 +16,6 @@ module.exports = function(app) {
   app.use("/api/movies", movies);
   app.use("/api/users", users);
   app.use("/api/auth", auth);
+  app.use("/api/returns", returns);
   app.use(error);
 };
