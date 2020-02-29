1d8e2a87d8b09001bbf2d6fc27d5252275f30db7
Abhishek Jain <abhishekjai35@gmail.com>
2020-02-29 21:12:22 +0530

    App now returns returnDate if input is valid

--- tests/Integration/returns.test.js
+++ tests/Integration/returns.test.js
@@ -67,14 +67,25 @@ describe("/api/returns", () => {
     const res = await exec();
     expect(res.status).toBe(404);
   });
+
   it("should return 400 if return is already processed", async () => {
     rental.dateReturned = new Date();
     await rental.save();
     const res = await exec();
     expect(res.status).toBe(400);
   });
+
   it("should return 200 if we have a valid request", async () => {
     const res = await exec();
     expect(res.status).toBe(200);
+  });
+
+  it("should set the return date if input is valid", async () => {
+    const res = await exec();
+
+    const rentalInDb = await Rental.findById(rental._id);
+    const diff = new Date() - rentalInDb.dateReturned;
+
+    expect(diff).toBeLessThan(10 * 1000);
   });
 });
