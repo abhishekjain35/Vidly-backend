2d2a78e5801628a572bc09493c5a2cb60e0b51c2
Abhishek Jain <abhishekjai35@gmail.com>
2020-02-29 20:49:23 +0530

    Implemented 404 error for customer/movie not found

--- routes/returns.js
+++ routes/returns.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { Rental } = require("../models/rentals");
 
 router.post("/", async (req, res) => {
-    if (!req.body.customerId) {
-        res.status(400).send('Customer id is not provided')
-    }
-    if (!req.body.movieId) {
-        res.status(400).send('movie id is not provided')
-    }
+  if (!req.body.customerId) {
+    res.status(400).send("Customer id is not provided");
+  }
+  if (!req.body.movieId) {
+    res.status(400).send("movie id is not provided");
+  }
+  const rental = await Rental.findOne({
+    "customer._id": req.body.customerId,
+    "movie._id": req.body.movieId
+  });
+  if (!rental) {
+    return res.status(404).send("Rental not Found");
+  }
   res.status(401).send("Unauthorized");
 });
 
--- tests/Integration/returns.test.js
+++ tests/Integration/returns.test.js
@@ -1,7 +1,6 @@
 const request = require("supertest");
 const { Rental } = require("../../models/rentals");
 const { User } = require("../../models/users");
-
 const mongoose = require("mongoose");
 
 describe("/api/returns", () => {
@@ -9,11 +8,20 @@ describe("/api/returns", () => {
   let customerId;
   let movieId;
   let rental;
+  let token;
+
+  const exec = () => {
+    return request(server)
+      .post("/api/returns")
+      .set("x-auth-token", token)
+      .send({ customerId, movieId });
+  };
 
   beforeEach(async () => {
     server = require("../../index");
     customerId = mongoose.Types.ObjectId();
     movieId = mongoose.Types.ObjectId();
+    token = new User().generateAuthToken();
 
     rental = new Rental({
       customer: {
@@ -36,30 +44,27 @@ describe("/api/returns", () => {
   });
 
   it("should return 401 if client is not logged in", async () => {
-    const res = await request(server)
-      .post("/api/returns")
-      .send({ customerId, movieId });
+    token = "";
+    const res = await exec();
 
     expect(res.status).toBe(401);
   });
 
   it("should return 400 if customer id is not provided", async () => {
-    const token = new User().generateAuthToken();
-    const res = await request(server)
-      .post("/api/returns")
-      .set("x-auth-token", token)
-      .send({ movieId });
-
+    customerId = "";
+    const res = await exec();
     expect(res.status).toBe(400);
   });
 
-  it("should return 400 if movie id is not provided", async () => {
-    const token = new User().generateAuthToken();
-    const res = await request(server)
-      .post("/api/returns")
-      .set("x-auth-token", token)
-      .send({ customerId });
-
+  it("should return 404 if movie id is not provided", async () => {
+    movieId = "";
+    const res = await exec();
     expect(res.status).toBe(400);
+  });
+
+  it("should return 404 if no rental found for the customer/movie", async () => {
+    Rental.remove({})
+    const res = await exec();
+    expect(res.status).toBe(404);
   });
 });
